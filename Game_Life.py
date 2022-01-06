866eebe341dacca0fc22cd420a2b2ca8f8168201
LilPomidorLil <rozhinthe@ya.ru>
2022-01-06 14:01:51 +0300

    добавлен метод рисования поля

--- Game_Life.py
+++ Game_Life.py
@@ -5,6 +5,9 @@ from pygame.locals import QUIT ## при нажатии на выход рабо
 
 ## создадим класс, в котором будет вся логика и интерфейс
 class GameLife:
+    """
+    Class of Game
+    """
     def __init__(self, width: int = 800, height: int = 800, cell_size: int = 10, speed: int = 10) -> None:
         """
         Создаем окно, в котором будет отображаться графика
@@ -14,15 +17,15 @@ class GameLife:
         width: int64 - ширина окна (пикс.), по умолч. 800
         height: int64 - высота она (пикс.), по умолч. 800
         cell_size: int64 - высота и ширина игровой клетки (пикс.), по умолч. 10
-        speed: int64 - скорость протекания игры
+        speed: int64 - скорость протекания игры, по умолч. 10
         """
         self.width = width
-        self.heght = height
+        self.height = height
         self.cell_size = cell_size
 
         # создадим переменные, отвечающие за кол-во клеток по горизонтали и вертикали
         self.w = self.width // self.cell_size
-        self.h = self.heght // self.cell_size
+        self.h = self.height // self.cell_size
 
         # зададим размеры окна
         self.resolution = width, height
@@ -35,4 +38,13 @@ class GameLife:
         self.next_generation_grid = np.zeros(shape=(self.w, self.h))
 
         self.speed = speed
-        
+
+    def displaying_lines(self) -> None:
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.color('white'), (x, 0), (x, self.height))
+
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('white'), (0, y), (self.width, y)
+    
+    
+
